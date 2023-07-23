c8f73c068ab3c7ffd6072ae859c1440c49b3586d
Sampathk23 <sampathk798@gmail.com>
2023-07-24 00:31:42 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,6 @@
 import React,{useState} from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import {Link} from 'react-scroll'
-import Typed from 'react-typed'
 import Porfolio from '../assets/Portfolio-bg.jpg'
 
 const Home = () => {
@@ -14,8 +13,7 @@ const Home = () => {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#66FCF1]'>Sampath Kumar</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a 
-            <Typed className='pl-4' strings={['Front','Back']} typeSpeed={120} backSpeed={140} loop/>End Developer</h2>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
             <p className='text-[#c5c6c7] max-w-[700px] py-4'>Welcome to my digital world! 🚀 A BBM grad turned tech enthusiast 🎓 | 
             From risk analyst to coding maestro 💻 | Crafting immersive web experiences 🌐 | A MERN Stack magician 🧙‍♂️ | 
             Embracing innovation & impacting lives ✨ | Let's code the future together! 🚀.</p>
